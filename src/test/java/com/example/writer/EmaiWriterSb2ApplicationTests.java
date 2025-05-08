1d68524dd44bda543a790dcb64c3489a1f15bbf0
Shreyansh Bansal <103424363+shreyansh3000@users.noreply.github.com>
2025-05-08 09:49:45 +0530

    initial commit

---
+++ src/test/java/com/example/writer/EmaiWriterSb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaiWriterSb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
